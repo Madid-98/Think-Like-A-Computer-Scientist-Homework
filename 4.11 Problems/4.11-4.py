de34c10a5c12f7a092c80067e3d1961c1a2a6b92
Madid-98 <madidabumirar@gmail.com>
2018-04-16 14:45:33 -0700

    Square error
    Square instead of square root

--- 4.11 Problems/4.11-4.py
+++ 4.11 Problems/4.11-4.py
@@ -15,7 +15,7 @@ print("B) Write a loop that prints each number and its square on a new line. ")
 print("Here is my solution")
 
 for i in [12, 10, 32, 3, 66, 17, 42, 99, 20]:
-    sqr = int(math.sqrt(i))
-    print("For the value of ", i, " the square root value is ", sqr)
+    sqr = float (i*i)
+    print("For the value of ", i, " the value squared ", sqr)
 
 print("That is everything :)")
